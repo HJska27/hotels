a7e79f2e5dfe4284475584e76b278bb3fcb76af5
Asian Himothy <evan.yang23@student.cart.org>
2023-11-01 09:27:29 -0700

    .

--- script.js
+++ script.js
@@ -8,11 +8,24 @@ window.onmousemove = e => {
           maxDelta = window.innerWidth / 2;
 
     const percentage = (mouseDelta / maxDelta) * -100,
-          nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+          nextPercentage = Math.max(Math.min(parseFloat(track.dataset.prevPercentage) + percentage, 0), -100);
 
+    
     track.dataset.percentage = nextPercentage;
 
     track.style.transform = `translate(${nextPercentage}%, -50%)`
+
+    track.animate({
+        transform: `translate(${nextPercentage}%, -50%)`
+    }, {duration: 1200, fill: "forwards"});
+
+    for(const image of track.getElementsByClassName("image")) {
+        image.style.objectPosition = `${nextPercentage + 100}% 50%`;
+        image.animate({
+            objectPosition: `${nextPercentage + 100}% center`
+        }, {duration: 1200, fill: "forwards"});
+    }
+
 }
 
 window.onmousedown = e => {
